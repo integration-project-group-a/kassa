5d7ac3765a28113e68c74705f2b8f81ce6b7550b
XMrCatX <44335993+XMrCatX@users.noreply.github.com>
2019-04-04 16:25:58 +0200

    Xml & Json over rabbitMQ

---
+++ Kassa/TaskCreator/Sender.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+class Sender
+{
+    public static void Main(string[] args)
+    {
+        
+            var factory = new ConnectionFactory() { HostName = "10.3.56.27", UserName = "manager", Password = "ehb" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "logs", type: "fanout");
+
+                var message = GetMessage(args);
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(exchange: "logs",
+                                     routingKey: "",
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            string xml = @"<?xml version='1.0'?>
+                      <person id='1'>
+                        <name>Alan</name>
+                        <url>http://www.google.com</url>
+                      </person>";
+            return ((args.Length > 0)
+                   ? string.Join(" ", args)
+                   : xml);
+        }
+    }
---
+++ Kassa/Worker/Receiver.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using Newtonsoft.Json;
+using System.Xml;
+
+class Receiver
+{
+    public static void Main()
+    {
+        var factory = new ConnectionFactory() { HostName = "10.3.56.27", UserName = "manager", Password = "ehb" };
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.ExchangeDeclare(exchange: "logs", type: "fanout");
+
+            var queueName = channel.QueueDeclare().QueueName;
+            channel.QueueBind(queue: queueName,
+                              exchange: "logs",
+                              routingKey: "");
+
+            Console.WriteLine(" [*] Waiting for logs.");
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body;
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(" [x] {0}", message);
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml(message);
+                string jsonText = JsonConvert.SerializeXmlNode(doc);
+                Console.WriteLine("Json:" + jsonText);
+            };
+            channel.BasicConsume(queue: queueName,
+                                 autoAck: true,
+                                 consumer: consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
